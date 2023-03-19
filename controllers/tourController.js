cf1682f7de22f8fb865ddd6f44d17ad9b1007bc4
Lê Minh Nghĩa <75244045+nghialmgcd201527@users.noreply.github.com>
2023-03-19 18:22:01 +0700

    API Fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -16,12 +16,19 @@ exports.getAllTours = async (req, res) => {
     let query = Tour.find(JSON.parse(queryStr));
 
     //2)SORTINGs
-
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
     } else {
       query = query.sort('-createdAt');
+    }
+
+    //3) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
     }
 
     //EXECUTE QUERY
